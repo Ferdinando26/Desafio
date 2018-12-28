03080cb9e4d166c937ea3dfdf92c2103ad104050
Lucas <lucashalves26@gmail.com>
2018-12-28 18:07:17 -0200

    criando o projeto

---
+++ rest-notas/src/com/accenture/java/desafio/entidade/Nota.java
@@ -0,0 +1,33 @@
+package com.accenture.java.desafio.entidade;
+
+public class Nota {
+
+	private int id;
+	private String descricao;
+	private String titulo;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+}
