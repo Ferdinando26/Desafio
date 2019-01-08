7ec49844600a958089b2f30c3bb085a2d5c839dd
Lucas <lucashalves26@gmail.com>
2019-01-08 10:52:24 -0200

    Project working

--- rest-notas/src/br/edu/devmedia/entidade/Nota.java
+++ rest-notas/src/br/edu/devmedia/entidade/Nota.java
@@ -1,10 +1,12 @@
-package com.accenture.java.desafio.entidade;
+package br.edu.devmedia.entidade;
 
 public class Nota {
 
 	private int id;
-	private String descricao;
+
 	private String titulo;
+
+	private String descricao;
 
 	public int getId() {
 		return id;
@@ -14,20 +16,20 @@ public class Nota {
 		this.id = id;
 	}
 
-	public String getDescricao() {
-		return descricao;
-	}
-
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-
 	public String getTitulo() {
 		return titulo;
 	}
 
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
 	}
 
 }
